ccc78711f231d0d8c8eff1466ca8c8d523ba8466
Mazaya <161602197+MazayaSRahmani@users.noreply.github.com>
2024-09-18 11:49:16 +0700

    Add files via upload

---
+++ Day 01_Band Name Generator.py
@@ -0,0 +1,26 @@
+# program 1 - making a band name generator
+
+print("Hello " + input("What's your name? ") + "!")
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in? \n")
+pet = input("What's the name of your pet? \n")
+
+print("Your band name could be "+ city + " "+ pet)
+
+
+# Note 1
+# You can swap the contents of a variable by adding a third temporary variable
+# Example:
+glass1 = "Milk"
+glass2 = "Juice"
+
+# Swap the contents
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print(glass1)
+print(glass2)
+
+
